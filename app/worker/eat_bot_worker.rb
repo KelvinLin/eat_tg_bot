eb01a957fdcd21ac419ca1429e36491122c9b77b
Kelvin Lin <tinklevin@gmail.com>
2015-11-23 16:15:24 +0000

    update

--- app/worker/eat_bot_worker.rb
+++ app/worker/eat_bot_worker.rb
@@ -20,9 +20,9 @@ class EatBotWorker
             reply.text = ["Hello, #{message.from.first_name}",
                           "安安, #{message.from.first_name} 您今天吃了嗎?"
                           ].sample
-          when /吃貨日記/i
+          when "/calendar",/吃貨日記/i
             reply.text = "要看吃貨日記,請找耽擱,爾蚊,Wen...等人申請..請提供gmail."
-          when /近期行程/i
+          when "/schedule",/近期行程/i
             reply.text = "1. 2015.12.03 Abaddon(19:30~)
                       阿巴丼 大安大隊 C66、C99、C112 慶功宴 莫宰羊-大安台大店-羊肉爐/羊肉火鍋\n
                       2. 2015.12.05 ENL Abaddon 綠軍慶功宴(11:30~
